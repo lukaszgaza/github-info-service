73fd4ac9052b2b8969e0f16d8ee21115246bb9e6
gazal <lukasz.gaza@ig.com>
2017-11-05 20:16:23 +0100

    Spring boot project template

---
+++ src/main/java/net/lukaszgaza/web/github/githubrestservice/GithubRestServiceApplication.java
@@ -0,0 +1,12 @@
+package net.lukaszgaza.web.github.githubrestservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubRestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubRestServiceApplication.class, args);
+	}
+}
